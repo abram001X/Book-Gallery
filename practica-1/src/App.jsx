2eaa138f931ff4e3419e99543a0114d9b1e98c00
Abraham <abrahamalfonzo11gmail.com>
2024-04-15 10:58:19 -0400

    Contador de libros aplicado

--- practica-1/src/App.jsx
+++ practica-1/src/App.jsx
@@ -3,34 +3,46 @@ import "./App.css";
 import result from "./books.json";
 import { BookGallery } from "./components/BookGallery";
 import { Menu } from "./components/Menu";
- // Crear galeria de libros
-
 
 function App() {
   const book = result.library.map((data) => ({ ...data.book, bolean: true }));
   const [books, setBooks] = useState(book);
-  //book seguro 
-  const  [booksSave,setBooksSave] = useState(book);
-  const counter = useRef(0);
+  const [booksSave, setBooksSave] = useState(book); //book seguro
+
   
-  booksSave.map(book=>{
-    if(book.bolean){
-      counter.current++
+  let countBookGenre = 0;
+  let bookAvailable = 0;
+  let bookNoAvailable = 0;
+
+//Guardar los valores en localStorage
+
+
+
+//contar cuantos true tienen los libros
+  booksSave.map((book) => {
+    if (book.bolean) {
+      bookAvailable++;
+    } else {
+      bookNoAvailable++;
     }
-  })
+  });
+
+  books.map(()=> {
+      countBookGenre++;
+  });
 
   // Detectar cuales libros están disponibles o no para leer
   const bookLearn = (selectedISBN) => {
-
-    
-    setBooksSave(booksSave.map( book=>{
-      if (book.ISBN === selectedISBN) {
-        return book.bolean
-          ? { ...book, bolean: false }
-          : { ...book, bolean: true };
-      }
-      return { ...book };
-    }))
+    setBooksSave(
+      booksSave.map((book) => {
+        if (book.ISBN === selectedISBN) {
+          return book.bolean
+            ? { ...book, bolean: false }
+            : { ...book, bolean: true };
+        }
+        return { ...book };
+      })
+    );
 
     setBooks(
       books.map((book) => {
@@ -42,27 +54,32 @@ function App() {
         return { ...book };
       })
     );
-    console.log(counter)
   };
 
+
   //Filtrar los libros, recorre books y tomar los libros que coincidan con los generos
-  
-  const booksFilter = genre =>{
+  const booksFilter = (genre) => {
     const books2 = booksSave;
-    if(genre == "Todos"){
-      return setBooks(books2)
+
+    if (genre == "Todos") {
+      return setBooks(books2);
     }
 
-    return setBooks(books2.filter(book => book.genre == genre))
-   
-  }
+    setBooks(books2.filter((book) => book.genre == genre));
+  };
 
- return ( 
-  <div className="content">
-    <Menu books={books} booksFilter={booksFilter}/>
-    <BookGallery books={books} bookLearn={bookLearn}/>
-    
-  </div>)
+  return (
+    <div className="content">
+      <Menu
+        books={books}
+        booksFilter={booksFilter}
+        bookAvailable={bookAvailable}
+        bookNoAvailable={bookNoAvailable}
+        countBookGenre={countBookGenre}
+      />
+      <BookGallery books={books} bookLearn={bookLearn} />
+    </div>
+  );
 }
 
 export default App;
--- practica-1/src/components/Menu.jsx
+++ practica-1/src/components/Menu.jsx
@@ -1,21 +1,27 @@
 //Menú de filtrados, categorias etc
 
 // eslint-disable-next-line react/prop-types
-export function Menu({booksFilter}) {
-
+export function Menu({ booksFilter, bookAvailable, bookNoAvailable, countBookGenre }) {
 
   return (
     <menu className="menu">
       <h1>BookGallery</h1>
       <div className="cont-filter">
-        <select name="Géneros" id="" onChange={e => booksFilter(e.target.value)}>
-        <option value="Todos">Todos</option>
+        <select
+          name="Géneros"
+          id=""
+          onChange={(e) => booksFilter(e.target.value)}
+        >
+          <option value="Todos">Todos</option>
           <option value="Fantasía">Fantasía</option>
           <option value="Ciencia ficción">Ciencia ficción</option>
           <option value="Terror">Terror</option>
           <option value="Zombies">Zombies</option>
         </select>
-        Libros Disponibles : {} Libros en lista de leídos : {}
+        <p>Libros disponibles : {bookAvailable}</p>
+         <p>Libros en lista de leídos :{bookNoAvailable}</p>
+         <p>Libros de este genero : {countBookGenre} </p>
+
       </div>
     </menu>
   );
